ffdc517569c628190143e85ec74d39413f39d5e7
Himanshu Paliwal <hpaliwal364@gmail.com>
2024-04-07 11:01:46 +0530

    some styling are done and responsive for mobile screen

--- script.js
+++ script.js
@@ -1,39 +1,58 @@
-let coin = document.querySelector(".coin");
-let flipBtn = document.querySelector("#flip-button");
-let coin_flip_sound = new Audio("coin_flip_sound.mp3");
+let coin = document.getElementById("coin");
+let flip_button = document.getElementById("flip_button");
+let result = document.getElementById("result");
+let number_of_heads = document.getElementById("number_of_heads");
+let number_of_tails = document.getElementById("number_of_tails");
+let coin_flip_sound = new Audio("./Assets/coin_flip_sound.mp3");
+let heads_count = 0;
+let tails_count = 0;
 
-flipBtn.addEventListener("click", () => {
-  document.querySelector("#result").innerHTML = "";
-  coin_flip_sound.play()
+// Flip coin logic
+flip_button.addEventListener("click", toss);
+
+// Flip coin function
+function toss() {
+  result.innerText = "";
+  coin_flip_sound.play();
   let i = Math.floor(Math.random() * 2);
   coin.style.animation = "none";
   if (i) {
     setTimeout(function () {
-      coin.style.animation = "spin-heads 3s forwards";
+      coin.style.animation = "spin-heads 4s forwards";
     }, 100);
-    setTimeout(Heads, 3000);
-  } 
-  else {
+    setTimeout(Heads, 4000);
+  } else {
     setTimeout(function () {
-      coin.style.animation = "spin-tails 3s forwards";
+      coin.style.animation = "spin-tails 4s forwards";
     }, 100);
-    setTimeout(Tails, 3000);
+    setTimeout(Tails, 4000);
   }
-  setTimeout(updateStats, 3000);
   disableButton();
-});
-
-function disableButton(){
-    flipBtn.disabled = true;
-    setTimeout(function(){
-        flipBtn.disabled = false;
-    },3000);
 }
 
+function disableButton() {
+  flip_button.disabled = true;
+  setTimeout(function () {
+    flip_button.disabled = false;
+  }, 4000);
+}
+
+// Funtion when heads comes
 function Heads() {
-  document.querySelector("#result").innerHTML = "Heads";
+  result.innerText = "Heads";
+  heads_count++;
+  setTimeout(() => {
+    number_of_heads.innerText = heads_count;
+    // number_of_tails.innerText = tails_count;
+  }, 200);
 }
 
+// Funtion when tails comes
 function Tails() {
-  document.querySelector("#result").innerHTML = "Tails";
+  result.innerText = "Tails";
+  tails_count++;
+  setTimeout(() => {
+    // number_of_heads.innerText = heads_count;
+    number_of_tails.innerText = tails_count;
+  }, 200);
 }
